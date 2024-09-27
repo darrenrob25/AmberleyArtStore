998cca9c55145e48b6a2c4bc3d5530e3f084ea88
darren robinson <darrenrobinsonpt@gmail.com>
2024-09-27 21:17:08 +0000

    Add: Basket functionality

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def basket_contents(request):
     """
@@ -9,6 +11,17 @@ def basket_contents(request):
     basket_items = []
     total_amount = 0
     item_count = 0
+    basket=request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total_amount += quantity * product.price
+        item_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     # Calculate delivery costs based on threshold
     if total_amount < settings.FREE_DELIVERY_THRESHOLD:
