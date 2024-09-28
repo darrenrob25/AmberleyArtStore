5e3aaaccf5c17c6538a58bbbffa75d95baa427f7
darren robinson <darrenrobinsonpt@gmail.com>
2024-09-28 16:39:28 +0000

    Add: Basket functionality

--- basket/views.py
+++ basket/views.py
@@ -1,75 +1,93 @@
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-
-# Create your views here.
+from django.contrib import messages
+from products.models import Product
 
 def view_basket(request):
     """ A view to view your basket """
-
-    return render(request,'basket/basket.html')
+    return render(request, 'basket/basket.html')
 
 
 def add_to_basket(request, item_id):
     """ Add products to basket """
+    quantity = int(request.POST.get('quantity', 0))
+    redirect_url = request.POST.get('redirect_url', reverse('view_basket'))  # Default to view_basket
 
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
+    
+    if quantity <= 0:
+        messages.error(request, "Invalid quantity. Please enter a valid number.")
+        return redirect(redirect_url)
 
-    if item_id in list(basket.keys()):
+    # Get product name for messages
+    try:
+        product = Product.objects.get(id=item_id)
+    except Product.DoesNotExist:
+        messages.error(request, "Product does not exist.")
+        return redirect(redirect_url)
+
+    if item_id in basket.keys():
         basket[item_id] += quantity
+        messages.success(request, f"Updated quantity of {product.name} in your basket.")
     else:
         basket[item_id] = quantity
+        messages.success(request, f"Added {product.name} to your basket.")
 
     request.session['basket'] = basket
-    print(request.session['basket'])
     return redirect(redirect_url)
 
 
 def adjust_basket(request, item_id):
     """Edit the basket."""
-    
-    # Get the basket from the session
     basket = request.session.get('basket', {})
     
-    # Check if the item exists in the basket
     if item_id not in basket:
-        # If the item does not exist, you may want to return an error response or redirect
-        return HttpResponse(status=404)
+        messages.error(request, "Item not found in your basket.")
+        return redirect('view_basket')  # Redirect to view basket
+
+    # Get product name for messages
+    try:
+        product = Product.objects.get(id=item_id)
+    except Product.DoesNotExist:
+        messages.error(request, "Product does not exist.")
+        return redirect('view_basket')  # Redirect to view basket
 
     try:
-        # Get the quantity from the POST data, default to 0 if not found
         quantity = int(request.POST.get('quantity', 0))
 
-        # If the quantity is greater than 0, update it
         if quantity > 0:
             basket[item_id] = quantity  # Set the new quantity
+            messages.success(request, f"Updated quantity of {product.name} in your basket.")
         else:
             basket.pop(item_id, None)  # Remove the item if quantity is 0
+            messages.success(request, f"Removed {product.name} from your basket.")
 
-        # Update the session with the modified basket
         request.session['basket'] = basket
-        redirect_url = request.POST.get('redirect_url', 'view_basket')  # Default to 'view_basket' if not provided
+        redirect_url = request.POST.get('redirect_url', 'view_basket')  # Default to 'view_basket'
         return redirect(redirect_url)
 
     except (ValueError, TypeError):
-        # Handle invalid quantity input
-        return HttpResponse(status=400)  # Bad request if the quantity isn't valid
+        messages.error(request, "Invalid quantity input.")
+        return redirect('view_basket')  # Redirect to view basket
 
 
 def remove_from_basket(request, item_id):
     """Remove an item from the basket and redirect."""
-    
-    # Get the basket from the session
     basket = request.session.get('basket', {})
 
-    # Safely remove the item from the basket, if it exists
+    # Get product name for messages
+    try:
+        product = Product.objects.get(id=item_id)
+    except Product.DoesNotExist:
+        messages.error(request, "Product does not exist.")
+        return redirect('view_basket')  # Redirect to view basket
+
     if item_id in basket:
         basket.pop(item_id)
+        messages.success(request, f"Removed {product.name} from your basket.")
+    else:
+        messages.error(request, "Item not found in your basket.")
 
-    # Update the session with the modified basket
     request.session['basket'] = basket
-
-    # Redirect back to the basket view
-    return redirect('view_basket')  # Make sure 'view_basket' is the name of your basket view URL
+    return redirect('view_basket')
