b878507679b4a4087af47519b9f0c5c2708ee0ff
darren robinson <darrenrobinsonpt@gmail.com>
2024-10-02 17:41:57 +0000

    Fix: Emails sending

--- amberley_artstore/settings.py
+++ amberley_artstore/settings.py
@@ -210,8 +210,3 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
-    # Ensure no keyfile or certfile is passed to starttls()
-    EMAIL_USE_SSL = False
-    EMAIL_SSL_CERTFILE = None
-    EMAIL_SSL_KEYFILE = None
