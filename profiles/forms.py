851f675ef2ff51d00d252984cf3f03f7a9973e53
darren robinson <darrenrobinsonpt@gmail.com>
2024-10-02 10:15:37 +0000

    Fix: Forms in user account

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,19 +1,12 @@
 from django import forms 
 from .models import UserProfile
-''' from .models import PurchaseOrder '''
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        exclude = (
-            'user',
-        )
+        exclude = ('default_user',)
 
     def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels, and set autofocus on the first field
-        """
         super().__init__(*args, **kwargs)
         placeholders = {
             'default_contact_number': 'Phone Number',
@@ -25,13 +18,10 @@ class UserProfileForm(forms.ModelForm):
             'default_state': 'State',
         }
 
-        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        self.fields['default_contact_number'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if field != 'default_country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'default_country' and field in placeholders:
+                placeholder = f'{placeholders[field]} *' if self.fields[field].required else placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form'
             self.fields[field].label = False
